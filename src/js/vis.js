bd37ae2ad3aa805a69ca995aa457f51f07d14d8f
lucasmonteiro001 <lucasmonteiro001@gmail.com>
2016-07-15 14:47:02 -0300

    Default values

--- src/js/vis.js
+++ src/js/vis.js
@@ -3,11 +3,14 @@
  */
 import Utilities from './modules/Utilities';
 import DOMElements from './modules/DOMElements';
+import DefaultValues from './modules/DefaultValues';
 
 global.$ = global.jQuery = require('jquery');
 
 let tr = DOMElements.new("tr");
 let th = DOMElements.new("th", {text: "cool", id: "myid", class: "btn btn-default"});
+
+alert(DefaultValues.MAX_COLS)
 
 $(document).ready(function() {
     console.log(tr);
